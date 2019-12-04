f023dd1f040d7f15bcb9f661d98760e5e418cb68
Tu-username <keysiwin25@gmail.com>
2019-12-03 21:29:44 -0500

    juego

--- ejercicio3/app.js
+++ ejercicio3/app.js
@@ -1,26 +1,62 @@
-// $(document).ready(function () {
+$(document).ready(function () {
     
-//   $('#botones').on('click', 'button', function (e) {
-//     console.log(e.target.id);
-//     $(`.${e.target.id}`)[0].pause();
-//     $(`.${e.target.id}`)[0].currentTime=0;
-//     $(`.${e.target.id}`)[0].play();
-//   });
+  $('#botones').on('click', 'button', function (e) {
+    console.log(e.target.id);
+    $(`.${e.target.id}`)[0].pause();
+    $(`.${e.target.id}`)[0].currentTime=0;
+    $(`.${e.target.id}`)[0].play();
+  });
 
-//   $(document).keyup(function () { 
-//     $(`.${event.key}`)[0].pause();
-//     $(`.${event.key}`)[0].currentTime=0;
-//     $(`.${event.key}`)[0].play();
+
+  
+//   $(document).keydown(function () { 
+//     console.log(event.which);
+//     if(event.which > 67 && event.which < 77 &&  !(event.which == 69 || event.which == 73))  {
+
+//         $(`audio[data-v=${event.key}]`)[0].pause();
+//         $(`audio[data-v=${event.key}]`)[0].currentTime=0;
+//         $(`audio[data-v=${event.key}]`)[0].play();
+//     }
 // });
 
-// });
 
-function sonido(nota) {
-    s = document.getElementsByClassName(nota);
-    s.addEventListener
-    document.getElementsByClassName(nota)[0].pause();
-    document.getElementsByClassName(nota)[0].currentTime=0;
-    document.getElementsByClassName(nota)[0].play();
+});
 
-}
+let s = document.getElementsByTagName('audio');
+console.log(s[1]);
+document.addEventListener('keypress' ,function(e) {
+        console.log(event.which);
+        $(`audio[data-v=${event.key}]`)[0].pause();
+        $(`audio[data-v=${event.key}]`)[0].currentTime=0;
+        $(`audio[data-v=${event.key}]`)[0].play();
+});
+
+
+
+console.log(s[1].dataset.v)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sonido(nota) {
+//     s = document.getElementsByClassName(nota);
+//     s.addEventListener
+//     document.getElementsByClassName(nota)[0].pause();
+//     document.getElementsByClassName(nota)[0].currentTime=0;
+//     document.getElementsByClassName(nota)[0].play();
+// }
 
